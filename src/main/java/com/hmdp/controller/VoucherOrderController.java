40928a4bd330cf818ca453934299b04be86dcf3e
Wolleaf Lee <744901920@qq.com>
2025-08-24 22:33:06 +0800

    实现水平分库分表

--- src/main/java/com/hmdp/controller/VoucherOrderController.java
+++ src/main/java/com/hmdp/controller/VoucherOrderController.java
@@ -1,13 +1,14 @@
 package com.hmdp.controller;
 
 
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
 import com.hmdp.domain.dto.Result;
+import com.hmdp.domain.entity.VoucherOrder;
 import com.hmdp.service.IVoucherOrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * <p>
@@ -28,5 +29,13 @@ public class VoucherOrderController {
     public Result seckillVoucher(@PathVariable("id") Long voucherId) {
         Long orderId = voucherOrderService.seckillVoucher(voucherId);
         return Result.success(orderId);
+    }
+
+    @GetMapping("{user-id}")
+    public Result queryVoucherOrderById(@PathVariable("user-id") Long userId) {
+        List<VoucherOrder> list = voucherOrderService.lambdaQuery()
+                .eq(VoucherOrder::getUserId, userId)
+                .list();
+        return Result.success(list);
     }
 }
